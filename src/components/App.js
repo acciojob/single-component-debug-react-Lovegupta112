1ab1f428c31b1a693dab5ae17e785f85e2eccc43
lovegupta112 <loveguptaofficials@gmail.com>
2023-08-06 22:32:28 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const App = () => {
   return (
-    <>
-      <p>
+    <div id="main">
+      <p >
         Now I can render any React component on any DOM node I want using
         ReactDOM.render
       </p>
-    </>
+    </div>
   );
 };
 
